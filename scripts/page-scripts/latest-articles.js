220a6a4b47a372535dbd821addb2657ccc0e5d76
unknown <S_Gravesen92@hotmail.com>
2018-03-14 10:19:46 +0100

    Add: Initial code (to be fixed)

--- scripts/page-scripts/latest-articles.js
+++ scripts/page-scripts/latest-articles.js
@@ -1,4 +1,99 @@
 function RunLatestArticles() {
-    // run code here
-    console.log("Latest Articles JS running.");
+
+var users = [ "francesco.agnoletto", "P1xt", "tropicalchancer", "jdmedlock" ]; // pre-defined users
+
+var totalRssCalls = users.length;
+var rssCalls = 0;
+
+var storiesToSort = [];
+
+    // Get RSS feeds functions
+    function GetRssFeeds(user) {
+        var data = {
+            rss_url: 'https://medium.com/feed/@' + user // Feed URL
+        };
+        $.get('https://api.rss2json.com/v1/api.json', data, function (response) { // Get feed
+            if (response.status == 'ok') {
+
+                var user = response.feed;
+                var stories = response.items;
+
+                var userObject = { 'userData':user, 'userStories':stories };
+
+                storiesToSort.push(userObject);
+
+                rssCalls++;
+
+            } else {
+                console.log(response.message);
+                rssCalls++;
+            }
+
+            if(rssCalls == totalRssCalls) { // Only call this function once all RSS Feeds are done
+                SortUserStories();
+            }
+
+        });
+    };
+
+    var test = 0;
+
+    // Sort users
+    function SortUserStories() {
+        var sortedArray = [];
+
+        storiesToSort.forEach(function(userObject) { // Loop through user objects - Userinfo + up to 10 stories
+            
+            userObject.userStories.forEach(function(userStory, i) { // Loop through stories - Individual story
+                var storyObj = { "userData": userObject.userData, "story": userStory };
+
+                if(sortedArray.length == 0) {
+                    sortedArray.push(storyObj);
+                }
+
+                else {
+                    sortedArray.forEach(function(sortedStoryObject, j) {                       
+                        if(new Date(storyObj.story.pubDate) > new Date(sortedStoryObject.story.pubDate)) { // no touch
+                            // test++;
+                            
+                            // sortedArray.splice(j, 0, storyObj);
+                            // return false;
+
+                        } else if(j == sortedArray.length-1) {
+                            // sortedArray.push(storyObj);
+                            // return false;
+
+                        }
+
+                    });
+
+                }
+                
+            });
+            
+        });
+
+        // console.log(sortedArray.length);
+        console.log(test);
+        
+        
+        
+    }
+
+
+    users.forEach(function(user) { // Get RSS Feeds from all users
+        GetRssFeeds(user);
+    });
+    
+    
+
+
+
+
 }
+
+
+
+
+
+// https://medium.com/feed/@tropicalchancer
