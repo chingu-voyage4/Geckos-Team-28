0b196614318c885c813d724371bbe2054d5b7929
Søren Gravesen <soreg@morningtrain.dk>
2018-03-28 22:21:32 +0200

    Added shared function to build HTML

--- scripts/page-scripts/featured-articles.js
+++ scripts/page-scripts/featured-articles.js
@@ -20,47 +20,9 @@ function RunFeaturedArticles() {
     
             // When all articles are in local array, run function to convert to HTML
             if(totalArticlesTransfered == totalFeatured) {
-                HandleFeaturedArticles();
+                BuildHtmlArticles(featuredArticles, 'featured', 'article article-featured', '.featured-articles-page .container');
             }
             
-        });
-    }
-
-    // Converts data to HTML
-    function HandleFeaturedArticles() {
-        
-        // Generate HTML for every element in array
-        $.each(featuredArticles, function(i, articleObject) {
-
-            // -- Start of build
-            
-            // build featured article container
-            $('<div />', {
-                id: 'featured-' + (i),
-                class: 'article article-featured'
-            }).appendTo('.featured-articles-page .container');
-
-            // build featured article child elements
-            $("#featured-" + i).html('\
-            <div class="article-container">\
-                <div class="image-container">\
-                    <img src="' + articleObject.articleImageLink + '" alt="Featured Article Image">\
-                </div>\
-                <div class="text-container">\
-                    <h3 class="article-heading">' + articleObject.articleHeading + '</h3>\
-                    <p class="article-summary">' + articleObject.articleSummary + '</p>\
-                    <div class="article-footer">\
-                        <a class="article-author" href="' + articleObject.articleAuthorLink + '" target="_blank">' + articleObject.articleAuthorName + '</a>\
-                    </div>\
-                </div>\
-                <div class="article-button">\
-                    <a href="' + articleObject.articleLink + '" target="_blank">Go to article</a>\
-                </div>\
-            </div>\
-            ');
-            
-            // --End of build
-
         });
     }
 
