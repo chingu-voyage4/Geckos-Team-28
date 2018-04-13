cabd40316c8b1970668169df3658e024d7021dd9
unknown <S_Gravesen92@hotmail.com>
2018-04-13 17:41:11 +0200

    Style: Modified styles of articles

--- scripts/shared-functions.js
+++ scripts/shared-functions.js
@@ -31,21 +31,18 @@ function BuildHtmlArticles(inputArray, idName, className, appendTo) {
 
         // build featured article child elements
         $('#' + idName + '-' + i).html('\
-        <div class="article-container">\
+        <a href="' + articleObject.articleLink + '" target="_blank" class="article-container">\
             <div class="image-container">\
                 <img src="' + imageLink + '" alt="Featured Article Image">\
             </div>\
             <div class="text-container">\
                 <h3 class="article-heading">' + articleObject.articleHeading + '</h3>\
                 <p class="article-summary">' + articleObject.articleSummary + '</p>\
-                <div class="article-footer">\
-                    <a class="article-author" href="' + articleObject.articleAuthorLink + '" target="_blank">' + articleObject.articleAuthorName + '</a>\
-                </div>\
+                    <div class="article-footer">\
+                        <p class="article-author">' + articleObject.articleAuthorName + '</p>\
+                    </div>\
             </div>\
-            <div class="article-button">\
-                <a href="' + articleObject.articleLink + '" target="_blank">Go to article</a>\
-            </div>\
-        </div>\
+        </a>\
         ');
         
         // --End of build
